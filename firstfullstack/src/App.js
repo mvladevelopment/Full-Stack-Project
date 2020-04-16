fa6cedc8d5a34fd3f6892e26dd916af5581bfe29
Monica Valencia <Monica.Valencia@emishealth.com>
2020-04-16 20:49:46 +0100

    test ci trigger 2

--- firstfullstack/src/App.js
+++ firstfullstack/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Sonar Cloud trial to see if this works now</h1>
       <h1>Sonar Cloud trial to see if this works OPEN test</h1>
     </div>
   );
