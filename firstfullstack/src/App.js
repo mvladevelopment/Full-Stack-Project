d54d566dd811700d563cbf7db20435135317474e
mvladevelopment <63799107+mvladevelopment@users.noreply.github.com>
2020-04-16 20:30:44 +0100

    Update App.js

--- firstfullstack/src/App.js
+++ firstfullstack/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Sonar Cloud trial to see if this works now</h1>
-      <h1>Sonar Cloud trial to see if this works final test</h1>
+      <h1>Sonar Cloud trial to see if this works open again test</h1>
     </div>
   );
 }
